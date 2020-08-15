5c8e8c3ff40edc1b11086dd759792ce33f9847a0
Faraz Shah <faraz.nisar1@gmail.com>
2020-08-15 14:33:30 +0530

    CleanUp

--- .history/src/App_20200815142646.js
+++ .history/src/App_20200815142646.js
@@ -1,12 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    M.AutoInit();
-  });
   return (
     <div className="App">
       <header className="App-header">
--- .history/src/App_20200815142912.js
+++ .history/src/App_20200815142912.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
 function App() {
   useEffect(() => {
-    M.AutoInit();
-  });
+    M.AutoInit()
+    }
+  })
   return (
     <div className="App">
       <header className="App-header">
